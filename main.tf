5e442118597b8972d20d3bc3a36c0c68e2af0785
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-01 17:36:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,57 +4,7 @@ provider "aws" {
   secret_key = "RD5Y3q6+M2oyF+oMztOojVQ/FE4eV0uPlbcxtxRw"
 }
 
-resource "aws_iam_role" "lambda_role" {
- name   = "terraform_aws_lambda_role"
- assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
 
-
-resource "aws_iam_policy" "iam_policy_for_lambda" {
-
-  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path         = "/"
-  description  = "AWS IAM Policy for managing aws lambda role"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-
-# Policy Attachment on the role.
-
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role        = aws_iam_role.lambda_role.name
-  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
-}
-
-# Generates an archive from content, a file, or a directory of files.
 
 data "archive_file" "zip_the_python_code" {
  type        = "zip"
@@ -62,8 +12,6 @@ data "archive_file" "zip_the_python_code" {
  output_path = "${path.module}/python/hello-python.zip"
 }
 
-# Create a lambda function
-# In terraform ${path.module} is the current directory.
 resource "aws_lambda_function" "terraform_lambda_func" {
  filename                       = "${path.module}/python/hello-python.zip"
  function_name                  = "Jhooq-Lambda-Function"
